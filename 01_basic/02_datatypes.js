497fceebf8f63ab1602c2066435d1aab26d18a8c
SK JASIMUL HAQUE <91880022+Jasimul786@users.noreply.github.com>
2024-12-04 15:57:03 +0000

    datatpes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+//alert(3+3) //we are using nodejs,not browser
+console.log(3
+    +3); //code should be readbility
+console.log("jasimul")
+let name="jasimul"
+let age=24
+let isloggedin=false
+
+//number => 2 to the power of 53
+//string=> we can use single quote('jas') and double quote("jas")
+//boolean =>true/false
+//null => standalone value
+//undefined => no value then show
+//symbol => unique
+
+
+//object
+
+console.log(typeof "jasimul")
+console.log(typeof null); //object
+console.log(typeof undefined) //undefined
